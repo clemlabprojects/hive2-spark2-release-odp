6a30500f545e747aad689399005eefcb42b85d97
Lucas Bakalian <lucas.bakalian@gmail.com>
2020-06-09 21:47:12 +0200

    FIX-2: HIVE2 SPARK add support for Hadoop 3

--- shims/common/src/main/java/org/apache/hadoop/hive/shims/ShimLoader.java
+++ shims/common/src/main/java/org/apache/hadoop/hive/shims/ShimLoader.java
@@ -170,6 +170,8 @@ public abstract class ShimLoader {
       return HADOOP20SVERSIONNAME;
     case 2:
       return HADOOP23VERSIONNAME;
+    case 3:
+      return HADOOP23VERSIONNAME;
     default:
       throw new IllegalArgumentException("Unrecognized Hadoop major version number: " + vers);
     }
